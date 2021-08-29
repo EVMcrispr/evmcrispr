484ab49bca3da75d2c1bc8d0b4815fd66524f088
PJColombo <paulo.colombo@pm.me>
2021-08-29 19:03:51 +0200

    Remove unused dev dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,10 @@
-import { utils } from "ethers";
 import fs from "fs";
 
 import "@nomiclabs/hardhat-waffle";
-import "@tenderly/hardhat-tenderly";
-import "@nomiclabs/hardhat-etherscan";
 import "hardhat-typechain";
-import "hardhat-deploy";
 import "hardhat-gas-reporter";
-import "solidity-coverage";
 
 import { task, HardhatUserConfig } from "hardhat/config";
-import { HttpNetworkUserConfig } from "hardhat/types";
-
-const { isAddress, getAddress, formatUnits, parseUnits } = utils;
-
-/*
-      📡 This is where you configure your deploy configuration for 🏗 scaffold-eth
-
-      check out `packages/scripts/deploy.js` to customize your deployment
-
-      out of the box it will auto deploy anything in the `contracts` folder and named *.sol
-      plus it will use *.args for constructor args
-*/
 
 //
 // Select the network you want to deploy to here:
@@ -72,20 +55,8 @@ const config: HardhatUserConfig = {
     outDir: "typechain",
     target: "ethers-v5",
   },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8",
-  },
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
-  },
-  namedAccounts: {
-    deployer: 0,
-  },
-  tenderly: {
-    project: process.env.TENDERLY_PROJECT || "",
-    username: process.env.TENDERLY_USERNAME || "",
   },
   networks: {
     hardhat: {
@@ -159,45 +130,6 @@ const config: HardhatUserConfig = {
     },
   },
 };
-
-const DEBUG = false;
-
-task("account", "Get balance informations for the deployment account.", async (_, { ethers }) => {
-  const hdkey = require("ethereumjs-wallet/hdkey");
-  const bip39 = require("bip39");
-  let mnemonic = fs.readFileSync("./mnemonic.txt").toString().trim();
-  if (DEBUG) console.log("mnemonic", mnemonic);
-  const seed = await bip39.mnemonicToSeed(mnemonic);
-  if (DEBUG) console.log("seed", seed);
-  const hdwallet = hdkey.fromMasterSeed(seed);
-  const wallet_hdpath = "m/44'/60'/0'/0/";
-  const account_index = 0;
-  let fullPath = wallet_hdpath + account_index;
-  if (DEBUG) console.log("fullPath", fullPath);
-  const wallet = hdwallet.derivePath(fullPath).getWallet();
-  const privateKey = "0x" + wallet._privKey.toString("hex");
-  if (DEBUG) console.log("privateKey", privateKey);
-  var EthUtil = require("ethereumjs-util");
-  const address = "0x" + EthUtil.privateToAddress(wallet._privKey).toString("hex");
-
-  var qrcode = require("qrcode-terminal");
-  qrcode.generate(address);
-  console.log("‍📬 Deployer Account is " + address);
-  for (let n in config.networks) {
-    //console.log(config.networks[n],n)
-    try {
-      let provider = new ethers.providers.JsonRpcProvider((config.networks[n] as HttpNetworkUserConfig).url);
-      let balance = await provider.getBalance(address);
-      console.log(" -- " + n + " --  -- -- 📡 ");
-      console.log("   balance: " + ethers.utils.formatEther(balance));
-      console.log("   nonce: " + (await provider.getTransactionCount(address)));
-    } catch (e) {
-      if (DEBUG) {
-        console.log(e);
-      }
-    }
-  }
-});
 
 task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
   const accounts = await ethers.provider.listAccounts();
