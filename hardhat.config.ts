b012d1e1dc7b6896c11f57a795c389b24fe54792
PJColombo <paulo.colombo@pm.me>
2021-09-13 13:41:02 +0200

    Restore `account` hardhat task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,9 @@ import "hardhat-typechain";
 import "hardhat-gas-reporter";
 
 import { task, HardhatUserConfig } from "hardhat/config";
+import { HttpNetworkUserConfig } from "hardhat/types";
+
+const DEBUG = false;
 
 //
 // Select the network you want to deploy to here:
@@ -81,7 +84,7 @@ const config: HardhatUserConfig = {
       allowUnlimitedContractSize: true,
     },
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", //<---- YOUR INFURA ID! (or it won't work)
+      url: "https://eth-rinkeby.alchemyapi.io/v2/zWeBt8YowIcK93EPCcp3862fvKTUsLDO", //<---- YOUR INFURA ID! (or it won't work)
       accounts: {
         mnemonic: mnemonic(),
       },
@@ -131,6 +134,42 @@ const config: HardhatUserConfig = {
   },
 };
 
+task("account", "Get balance informations for the deployment account.", async (_, { ethers }) => {
+  const hdkey = require("ethereumjs-wallet/hdkey");
+  const bip39 = require("bip39");
+  let mnemonic = fs.readFileSync("./mnemonic.txt").toString().trim();
+  if (DEBUG) console.log("mnemonic", mnemonic);
+  const seed = await bip39.mnemonicToSeed(mnemonic);
+  if (DEBUG) console.log("seed", seed);
+  const hdwallet = hdkey.fromMasterSeed(seed);
+  const wallet_hdpath = "m/44'/60'/0'/0/";
+  const account_index = 0;
+  let fullPath = wallet_hdpath + account_index;
+  if (DEBUG) console.log("fullPath", fullPath);
+  const wallet = hdwallet.derivePath(fullPath).getWallet();
+  const privateKey = "0x" + wallet._privKey.toString("hex");
+  if (DEBUG) console.log("privateKey", privateKey);
+  var EthUtil = require("ethereumjs-util");
+  const address = "0x" + EthUtil.privateToAddress(wallet._privKey).toString("hex");
+
+  var qrcode = require("qrcode-terminal");
+  qrcode.generate(address);
+  console.log("‍📬 Deployer Account is " + address);
+  for (let n in config.networks) {
+    //console.log(config.networks[n],n)
+    try {
+      let provider = new ethers.providers.JsonRpcProvider((config.networks[n] as HttpNetworkUserConfig).url);
+      let balance = await provider.getBalance(address);
+      console.log(" -- " + n + " --  -- -- 📡 ");
+      console.log("   balance: " + ethers.utils.formatEther(balance));
+      console.log("   nonce: " + (await provider.getTransactionCount(address)));
+    } catch (e) {
+      if (DEBUG) {
+        console.log(e);
+      }
+    }
+  }
+});
 task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
   const accounts = await ethers.provider.listAccounts();
   accounts.forEach((account) => console.log(account));
