398fe5b5323d09a9ecc72b9df20cb3bee46bb00f
David Llop <931684+sembrestels@users.noreply.github.com>
2021-10-14 02:02:47 +0200

    Fix toDecimals function for amount 0 with 0 decimals

--- src/helpers/web3.ts
+++ src/helpers/web3.ts
@@ -28,7 +28,7 @@ export function calculateNewProxyAddress(daoAddress: Address, nonce: string): Ad
 
 export const toDecimals = (amount: number | string, decimals = 18): BigNumber => {
   const [integer, decimal] = String(amount).split(".");
-  return BigNumber.from((integer != "0" ? integer : "") + (decimal || "").padEnd(decimals, "0"));
+  return BigNumber.from((integer != "0" ? integer : "") + (decimal || "").padEnd(decimals, "0") || "0");
 };
 
 export function getFunctionParams(functionName: string, abi: Interface): [string[], string[]] {
