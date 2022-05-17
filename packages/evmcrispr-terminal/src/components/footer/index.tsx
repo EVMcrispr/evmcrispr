78e8b1369aacdd195e58aebd37c842c80a59fb45
Viviane Dias <vivianedias@protonmail.com>
2022-05-17 15:45:49 -0300

    refactor: uses only chakra ui comp

--- packages/evmcrispr-terminal/src/components/footer/index.tsx
+++ packages/evmcrispr-terminal/src/components/footer/index.tsx
@@ -1,4 +1,5 @@
-import { Center, Text, VStack, Stack, Link } from '@chakra-ui/react';
+import { Center, Heading, Link, Stack, Text, VStack } from '@chakra-ui/react';
+
 import giveth from '../../assets/giveth.svg';
 import OneHive from '../../assets/1hive.svg';
 import curve from '../../assets/curve.svg';
@@ -31,19 +32,25 @@ const sponsors = [
 const Footer = () => {
   return (
     <>
-      <Center bgColor="rgba(24, 24, 24, 1)" p="50px">
+      <Center bgColor="rgba(24, 24, 24, 1)" p={14} as="section">
         <VStack spacing={12}>
-          <Text color="brand.green" align="center">
+          <Heading as="h2" color="brand.green" size="md" textAlign="center">
             <strong>EVMcrispr</strong> would have not been possible without the
             support of
-          </Text>
+          </Heading>
           <Stack
-            direction={{ base: "column", md: "column", lg: "row" }}
-            spacing={{ base: 20, md: 20, lg: 48}}
+            direction={{ base: 'column', md: 'column', lg: 'row' }}
+            justify="space-between"
             align="center"
+            width={{ md: '956px' }}
           >
-            {sponsors.map(({ src, link, name }) => (
-              <a href={link} target="_blank" rel="noreferrer">
+            {sponsors.map(({ src, link, name }, i) => (
+              <a
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+                key={`sponsors-link-${i}`}
+              >
                 <VStack spacing={10}>
                   <img src={src} alt={name} />
                   <Text color="white" textTransform="uppercase">
@@ -56,10 +63,14 @@ const Footer = () => {
         </VStack>
       </Center>
 
-      <Center as="footer">
+      <Center py={14}>
         <VStack spacing={6}>
           <Text color="brand.green">powered by Blossom</Text>
-          <Link href="https://github.com/blossomlabs" target="_blank" rel="noreferrer">
+          <Link
+            href="https://github.com/blossomlabs"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={Blossom} alt="Blossom" height="48px" />
           </Link>
         </VStack>
