b0a04524e71a9e557a16c2f55a31b8be8f442b90
PJColombo <paulo.colombo@pm.me>
2022-09-03 14:05:28 +0200

    chore: comment out scripts

--- packages/evmcrispr/scripts/examples/wrap.ts
+++ packages/evmcrispr/scripts/examples/wrap.ts
@@ -1,37 +1,40 @@
-import { Contract } from 'ethers';
+// TODO: update to cas11 evmcrispr
+// import { Contract } from 'ethers';
 
-import { EVMcrispr } from '../../src';
-import { impersonateAddress } from '../../helpers/rpc';
+// import { EVMcrispr } from '../../src';
+// import { impersonateAddress } from '../../helpers/rpc';
 
-const CHAIN_ID = 100;
+// const CHAIN_ID = 100;
 
-const DAO = '0x2050eabe84409e480ad1062001fdb6dfbc836192';
+// const DAO = '0x2050eabe84409e480ad1062001fdb6dfbc836192';
 
-const ADDRESS = '0x4355a2cdec902C372F404007114bbCf2C65A3eb0';
+// const ADDRESS = '0x4355a2cdec902C372F404007114bbCf2C65A3eb0';
 
-const main = async () => {
-  const signer = await impersonateAddress(ADDRESS);
+// const main = async () => {
+//   const signer = await impersonateAddress(ADDRESS);
 
-  signer.getChainId = async () => CHAIN_ID;
+//   signer.getChainId = async () => CHAIN_ID;
 
-  const evmcrispr = await EVMcrispr.create(signer);
-  const dao = await evmcrispr.aragon.dao(DAO);
+//   const evmcrispr = await EVMcrispr.create(signer);
+//   const dao = await evmcrispr.aragon.dao(DAO);
 
-  const tokenManagerApp = dao.app('wrappable-hooked-token-manager.open:0')!;
-  const tokenManager = new Contract(
-    tokenManagerApp.address,
-    tokenManagerApp.abiInterface,
-    signer,
-  );
+//   const tokenManagerApp = dao.app('wrappable-hooked-token-manager.open:0')!;
+//   const tokenManager = new Contract(
+//     tokenManagerApp.address,
+//     tokenManagerApp.abiInterface,
+//     signer,
+//   );
 
-  const txRecipt = await tokenManager.wrap('50000000000000000000');
+//   const txRecipt = await tokenManager.wrap('50000000000000000000');
 
-  console.log(JSON.stringify(txRecipt));
-};
+//   console.log(JSON.stringify(txRecipt));
+// };
 
-main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+// main()
+//   .then(() => process.exit(0))
+//   .catch((err) => {
+//     console.error(err);
+//     process.exit(1);
+//   });
+
+export {};
