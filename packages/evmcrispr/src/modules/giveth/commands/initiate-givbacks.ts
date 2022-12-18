a4a511e25ed2df58fb26abc9d07a2723ef8cae38
Sem <931684+sembrestels@users.noreply.github.com>
2022-12-18 00:00:40 -0300

    Add tests for giveth:initiate-givbacks command

--- packages/evmcrispr/src/modules/giveth/commands/initiate-givbacks.ts
+++ packages/evmcrispr/src/modules/giveth/commands/initiate-givbacks.ts
@@ -2,21 +2,30 @@ import { Contract } from 'ethers';
 
 import type { ICommand } from '../../../types';
 
-import { ComparisonType, checkArgsLength, encodeAction } from '../../../utils';
+import {
+  ComparisonType,
+  checkArgsLength,
+  checkOpts,
+  encodeAction,
+  getOptValue,
+} from '../../../utils';
 import { batchForwarderActions } from '../../aragonos/utils';
 
 import type { Giveth } from '../Giveth';
 
-const relayerAddr = '0xd0e81E3EE863318D0121501ff48C6C3e3Fd6cbc7';
+const defaultRelayerAddr = '0xd0e81E3EE863318D0121501ff48C6C3e3Fd6cbc7';
 const tokenManager = '0x3efac97efa6f237e67b4f8c616a194fd0583d99a';
 const voting = '0x30c9aa17fc30e4c23a65680a35b33e8f3b4198a2';
 const agent = '0x2fa20fa7fc404d35748497c0f28f8fb2f8731336';
 
 export const initiateGivbacks: ICommand<Giveth> = {
-  async run(module, c, { interpretNodes }) {
+  async run(module, c, { interpretNode, interpretNodes }) {
     checkArgsLength(c, { type: ComparisonType.Equal, minValue: 1 });
+    checkOpts(c, ['relayer']);
 
     const [hash] = await interpretNodes(c.args);
+    const relayerAddr =
+      (await getOptValue(c, 'relayer', interpretNode)) || defaultRelayerAddr;
 
     const data = await fetch('https://ipfs.blossom.software/ipfs/' + hash).then(
       (data) => data.json(),
