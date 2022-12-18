ef72659c2dae13dd1716afea8bae20d9b14433f1
Sem <931684+sembrestels@users.noreply.github.com>
2022-12-18 00:30:44 -0300

    Add tests to giveth:finalize-givbacks command

--- packages/evmcrispr/src/modules/giveth/commands/finalize-givbacks.ts
+++ packages/evmcrispr/src/modules/giveth/commands/finalize-givbacks.ts
@@ -1,19 +1,29 @@
 import type { ICommand } from '../../../types';
 
-import { ComparisonType, checkArgsLength, encodeAction } from '../../../utils';
+import {
+  ComparisonType,
+  checkArgsLength,
+  checkOpts,
+  encodeAction,
+  getOptValue,
+} from '../../../utils';
 
 import type { Giveth } from '../Giveth';
 
+const defaultRelayerAddr = '0xd0e81E3EE863318D0121501ff48C6C3e3Fd6cbc7';
+
 export const finalizeGivbacks: ICommand<Giveth> = {
-  async run(_, c, { interpretNodes }) {
+  async run(_, c, { interpretNode, interpretNodes }) {
     checkArgsLength(c, { type: ComparisonType.Equal, minValue: 1 });
+    checkOpts(c, ['relayer']);
 
     const [hash] = await interpretNodes(c.args);
+    const relayerAddr =
+      (await getOptValue(c, 'relayer', interpretNode)) || defaultRelayerAddr;
 
     const batches = await fetch(
       'https://ipfs.blossom.software/ipfs/' + hash,
     ).then((data) => data.json());
-    const relayerAddr = '0xd0e81E3EE863318D0121501ff48C6C3e3Fd6cbc7';
     return batches.map((batch: any) =>
       encodeAction(relayerAddr, 'executeBatch(uint256,address[],uint256[])', [
         batch.nonce,
