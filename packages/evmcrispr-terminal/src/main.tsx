db0d6c69b8d19c6c936d8cc21cf9bae7dbcbdfb0
Viviane Dias <vivianedias@pm.me>
2022-12-19 11:49:52 -0300

    chore(react): updates react to v18

--- packages/evmcrispr-terminal/src/main.tsx
+++ packages/evmcrispr-terminal/src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom/';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
 
-render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
