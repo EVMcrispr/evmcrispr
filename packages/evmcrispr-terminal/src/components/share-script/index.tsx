28cafbfd4bfb255e975c6821f242bb222271045a
Viviane Dias <vivianedias@pm.me>
2022-12-20 22:49:35 -0300

    feat(modal): renames share-modal to share-script

--- packages/evmcrispr-terminal/src/components/share-script/index.tsx
+++ packages/evmcrispr-terminal/src/components/share-script/index.tsx
@@ -1,10 +1,83 @@
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Icon, IconButton, useDisclosure, useToast } from '@chakra-ui/react';
-import { ShareIcon } from '@heroicons/react/24/solid';
+import {
+  Box,
+  Icon as ChakraIcon,
+  HStack,
+  Icon,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  VStack,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react';
+import { ShareIcon, Square2StackIcon } from '@heroicons/react/24/solid';
 
-import ShareModal from '../share-modal';
 import pinJSON from '../../api/pinata/pinJSON';
+
+const ShareModal = ({
+  isOpen,
+  onClose,
+  url,
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  url: string;
+  isLoading: boolean;
+}) => {
+  function handleClick() {
+    return navigator.clipboard.writeText(url);
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      colorScheme={'yellow'}
+      size={'xl'}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Share script</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <VStack>
+            <ChakraIcon as={ShareIcon} boxSize={24} color={'brand.green.300'} />
+            <Text color={'brand.yellow.300'} fontWeight={700}>
+              do you want to share this script?
+            </Text>
+            <HStack>
+              <Box
+                border={'1px solid'}
+                borderColor={'brand.green.300'}
+                w={'lg'}
+                bgColor={'gray'}
+              >
+                <Text color={'white'} maxWidth={'lg'} noOfLines={1}>
+                  {url}
+                </Text>
+              </Box>
+              <IconButton
+                icon={<ChakraIcon as={Square2StackIcon} />}
+                aria-label={'Copy link'}
+                onClick={handleClick}
+                variant={'outline'}
+                size={'md'}
+              />
+            </HStack>
+          </VStack>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
 
 export default function ShareButton({
   script,
