62a6202dba4dd740452ba6643963d5cda3b6079e
Viviane Dias <vivianedias@pm.me>
2022-12-21 10:51:40 -0300

    feat(utils): moves getRootLocation func to utils

---
+++ packages/evmcrispr-terminal/src/utils/location.ts
@@ -0,0 +1,7 @@
+export default function getRootLocation(hashId?: string) {
+  const url = window.location.href;
+  const urlArr = url.split('/');
+  const urlWithoutHash = urlArr.filter((u) => u !== hashId);
+
+  return urlWithoutHash.join('/');
+}
