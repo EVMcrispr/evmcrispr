e0c22be56a69decbf1b6ba9a9b4ed1401d4ce42c
PJColombo <paulo.colombo@pm.me>
2022-12-27 15:01:20 -0300

    chore: set up common eslint config for all packages

---
+++ common/eslint-config-evmcrispr/.eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+      experimentalObjectRestSpread: true,
+    },
+    allowImportExportEverywhere: true,
+  },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['external', 'internal'],
+        'newlines-between': 'always-and-inside-groups',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    'sort-imports': [
+      'warn',
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+    'spaced-comment': [
+      'error',
+      'always',
+      {
+        block: {
+          markers: ['*'],
+          balanced: true,
+        },
+      },
+    ],
+  },
+  ignorePatterns: ['node_modules', 'dist', 'CHANGELOG.md'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    mocha: true,
+  },
+};
