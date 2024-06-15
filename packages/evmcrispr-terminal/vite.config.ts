a350a94f549ea7ee9c3a70f869a6903fc5826805
Sem <931684+sembrestels@users.noreply.github.com>
2024-06-15 13:36:12 +0200

    Make manifest.json compatible with Safe

--- packages/evmcrispr-terminal/vite.config.ts
+++ packages/evmcrispr-terminal/vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   define: {
     global: 'globalThis',
   },
   plugins: [react()],
+  server: {
+    port: 3000,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization',
+    },
+  },
 });
