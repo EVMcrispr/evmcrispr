aae55b2373a30bdfd457d78235f6abe9222fe11f
Sem <931684+sembrestels@users.noreply.github.com>
2024-06-21 14:55:16 +0200

    Use buffer polyfill instead of node ones

--- packages/evmcrispr-terminal/src/main.tsx
+++ packages/evmcrispr-terminal/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './polyfills';
 
 import App from './App';
 import './index.css';
--- packages/evmcrispr-terminal/vite.config.ts
+++ packages/evmcrispr-terminal/vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     global: 'globalThis',
   },
-  plugins: [
-    react(),
-    NodeGlobalsPolyfillPlugin({
-      buffer: true,
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
     headers: {
